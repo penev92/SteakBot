3227473fed57ce4a102c0dccede1754b9de5f574
Pavel Penev <penev92@gmail.com>
2019-02-01 03:59:56 +0200

    Refactored the Bot class

--- SteakBot.Core/Bot.cs
+++ SteakBot.Core/Bot.cs
@@ -14,73 +14,51 @@ namespace SteakBot.Core
 	{
 		private static readonly string discordBotToken = "";
 
-		private CommandService _commands;
-		private AudioService _audioService;
-		private DiscordSocketClient _client;
-		private IServiceProvider _services;
+		private readonly DiscordSocketClient _client;
+		private readonly CommandService _commands;
+		private readonly IServiceProvider _serviceProvider;
+
+		public Bot()
+		{
+			_client = new DiscordSocketClient();
+			_commands = new CommandService();
+			_serviceProvider = new ServiceCollection()
+				.AddSingleton(_client)
+				.AddSingleton(_commands)
+				.AddSingleton<AudioService>()
+				.BuildServiceProvider();
+
+			AttachEventHandlers();
+			RegisterCommandModules();
+		}
 
 		public async Task RunAsync()
 		{
-			_client = new DiscordSocketClient();
-			_client.Log += Log;
-
-			_client.ReactionAdded += HandleReactionAdded;
-
-			_commands = new CommandService();
-			_audioService = new AudioService();
-
 			await _client.LoginAsync(TokenType.Bot, discordBotToken);
 			await _client.StartAsync();
-
-			_services = new ServiceCollection()
-				.AddSingleton(_client)
-				.AddSingleton(_commands)
-				.AddSingleton(_audioService)
-				.BuildServiceProvider();
-
-			await InstallCommandsAsync();
 
 			Console.ReadLine();
 
 			await _client.LogoutAsync();
 			await _client.StopAsync();
 
-			Console.ReadLine();
-
 			Console.WriteLine("Done");
-
-			// Block this task until the program is closed.
-			await Task.Delay(-1);
-
-			//var webhookClient = new Discord.Webhook.DiscordWebhookClient(123, "", new DiscordRestConfig());
-			//webhookClient.
+			Console.ReadLine();
 		}
 
 		private async Task HandleReactionAdded(Cacheable<IUserMessage, ulong> arg1, ISocketMessageChannel arg2, SocketReaction arg3)
 		{
 			var channel = arg3.Channel;
-			var message = await channel.GetMessageAsync(arg3.MessageId, CacheMode.AllowDownload);
+			var message = await channel.GetMessageAsync(arg3.MessageId);
 
 			await channel.SendMessageAsync($"{arg3.User.Value.Mention} reacted with {arg3.Emote} to a message by {message.Author.Username}"
 				+ $" from {message.CreatedAt.LocalDateTime.ToString("HH:mm:ss")} of {message.CreatedAt.LocalDateTime.ToString("dd.MM.yyyy")}.");
-
-			return;
 		}
 
 		private Task Log(LogMessage msg)
 		{
 			Console.WriteLine(msg.ToString());
 			return Task.CompletedTask;
-		}
-
-		public async Task InstallCommandsAsync()
-		{
-			// Hook the MessageReceived Event into our Command Handler
-			_client.MessageReceived += HandleCommandAsync;
-			_client.UserVoiceStateUpdated += HandleUserVoiceStateUpdated;
-
-			// Discover all of the commands in this assembly and load them.
-			await _commands.AddModulesAsync(Assembly.GetExecutingAssembly(), _services);
 		}
 
 		private async Task HandleUserVoiceStateUpdated(SocketUser user, SocketVoiceState leaveState, SocketVoiceState joinState)
@@ -134,7 +112,7 @@ namespace SteakBot.Core
 
 				// Execute the command. (result does not indicate a return value, 
 				// rather an object stating if the command executed successfully)
-				var result = await _commands.ExecuteAsync(context, argPos, _services);
+				var result = await _commands.ExecuteAsync(context, argPos, _serviceProvider);
 				if (!result.IsSuccess)
 				{
 					await context.Channel.SendMessageAsync(result.ErrorReason);
@@ -144,7 +122,25 @@ namespace SteakBot.Core
 
 		public void Dispose()
 		{
+			((IDisposable) _commands)?.Dispose();
 			_client?.Dispose();
 		}
+
+		#region Private methods
+
+		private void AttachEventHandlers()
+		{
+			_client.Log += Log;
+			_client.ReactionAdded += HandleReactionAdded;
+			_client.MessageReceived += HandleCommandAsync;
+			_client.UserVoiceStateUpdated += HandleUserVoiceStateUpdated;
+		}
+
+		private void RegisterCommandModules()
+		{
+			_commands.AddModulesAsync(Assembly.GetExecutingAssembly(), _serviceProvider).Wait();
+		}
+
+		#endregion
 	}
 }
