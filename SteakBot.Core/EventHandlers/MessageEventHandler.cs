bc61bd5e9191f893c5565871782163317480e946
naps250 <naps250@gmail.com>
2019-02-10 15:59:07 +0200

    This took me 30mins starter pack.

--- SteakBot.Core/EventHandlers/MessageEventHandler.cs
+++ SteakBot.Core/EventHandlers/MessageEventHandler.cs
@@ -18,21 +18,21 @@ namespace SteakBot.Core.EventHandlers
 			_customMessageHandlers = serviceProvider.GetServices<ICustomMessageHandler>();
 		}
 
-		public async Task HandleMessageReceivedAsync(SocketMessage messageParam)
-		{
-			// Don't process the command if it was a System Message, the sender is a bot or this is not a command.
-			if (!(messageParam is SocketUserMessage message) || message.Source == MessageSource.Bot)
-			{
-				return;
-			}
+        public async Task HandleMessageReceivedAsync(SocketMessage messageParam)
+        {
+            // Process the command if it is a User Message and the sender is not a bot.
+            if (messageParam is SocketUserMessage message && messageParam.Source != MessageSource.Bot)
+            {
+                Parallel.ForEach(_customMessageHandlers, customMessageHandler =>
+                {
+                    if (customMessageHandler.CanHandle(message))
+                    {
+                        customMessageHandler.Invoke(message);
+                    }
+                });
+            }
 
-			Parallel.ForEach(_customMessageHandlers, customMessageHandler =>
-			{
-				if (customMessageHandler.CanHandle(message))
-				{
-					customMessageHandler.Invoke(message);
-				}
-			});
-		}
-	}
+            await Task.CompletedTask;
+        }
+    }
 }
