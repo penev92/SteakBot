252f1b7b9d6b12c133511e9cd80aba8231430a88
Atanas <atanas.bozhanin@gmail.com>
2019-02-16 21:21:19 +0200

    * added mapping voice to text channel * filtered to only join/leave/move events

--- SteakBot.Core/EventHandlers/VoiceStateEventHandler.cs
+++ SteakBot.Core/EventHandlers/VoiceStateEventHandler.cs
@@ -1,33 +1,64 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
-using Discord.WebSocket;
 using SteakBot.Core.EventHandlers.Abstraction;
+using Discord.WebSocket;
 
 namespace SteakBot.Core.EventHandlers
 {
     internal class VoiceStateEventHandler : IVoiceStateEventHandler
     {
-        public async Task HandleUserVoiceStateUpdatedAsync(SocketUser user, SocketVoiceState leaveState, SocketVoiceState joinState)
+        // TODO: Get from configuration.
+        private const string FallbackMessageChannel = "bendoverwatch";
+
+        // TODO: Get from configuration.
+        private readonly IReadOnlyDictionary<string, string> _voiceToMessageChannelMapping = new Dictionary<string, string>
         {
-            if (joinState.VoiceChannel != null)
+            { "shavilandia", "bendoverwatch" },
+            { "starcraft_voice", "zurg_op" },
+            { "random_voice", "parjoli" },
+            { "play_diablo_here_pliz", "general_gaming" }
+        };
+
+        public async Task HandleUserVoiceStateUpdatedAsync(SocketUser user, SocketVoiceState oldState, SocketVoiceState newState)
+        {
+            // Join
+            if (oldState.VoiceChannel == null && newState.VoiceChannel != null)
             {
-                // TODO: Perhaps not hardcode this so?
-                var channel = joinState.VoiceChannel.Guild.Channels.FirstOrDefault(x => x.Name == "bendoverwatch");
-                if (channel is ISocketMessageChannel messageChannel)
-                {
-                    await messageChannel.SendMessageAsync($"{user.Mention} has joined {joinState.VoiceChannel.Name}");
-                }
+                await SendMessageAsync(newState, $"{user.Mention} has joined {newState.VoiceChannel.Name}");
             }
-            else
-            if (leaveState.VoiceChannel != null)
+            // Leave
+            else if (oldState.VoiceChannel != null && newState.VoiceChannel == null)
             {
-                // TODO: Perhaps not hardcode this so?
-                var channel = leaveState.VoiceChannel.Guild.Channels.FirstOrDefault(x => x.Name == "bendoverwatch");
-                if (channel is ISocketMessageChannel messageChannel)
-                {
-                    await messageChannel.SendMessageAsync($"{user.Mention} has ragequit {leaveState.VoiceChannel.Name}");
-                }
+                await SendMessageAsync(oldState, $"{user.Mention} has ragequit {oldState.VoiceChannel.Name}");
             }
+            // Change
+            else if (oldState.VoiceChannel != null && newState.VoiceChannel != null
+                     && oldState.VoiceChannel.Name != newState.VoiceChannel.Name)
+            {
+                await SendMessageAsync(newState, $"{user.Mention} has moved from {oldState.VoiceChannel.Name} to {newState.VoiceChannel.Name}");
+            }
+        }
+
+        private async Task SendMessageAsync(SocketVoiceState state, string message)
+        {
+            var messageChannelName = GetMessageChannelForVoiceEvent(state.VoiceChannel.Name);
+            var channel = state.VoiceChannel.Guild.Channels.FirstOrDefault(x => x.Name == messageChannelName);
+            if (channel is ISocketMessageChannel messageChannel)
+            {
+                await messageChannel.SendMessageAsync(message);
+            }
+        }
+
+        private string GetMessageChannelForVoiceEvent(string voiceChannelName)
+        {
+            if (_voiceToMessageChannelMapping.ContainsKey(voiceChannelName))
+            {
+                return _voiceToMessageChannelMapping[voiceChannelName];
+            }
+
+            // Fallback
+            return FallbackMessageChannel;
         }
     }
 }
