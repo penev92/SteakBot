1e9bb8d4c6253d68e6eb7caf6ae9b4222e1361d5
Pavel Penev <penev92@gmail.com>
2019-10-12 15:18:42 +0300

    DUMP 2

--- SteakBot.Core/Services/AudioService.cs
+++ SteakBot.Core/Services/AudioService.cs
@@ -1,8 +1,7 @@
-﻿using System.Collections.Concurrent;
-using System.Collections.Generic;
+﻿using System;
+using System.Collections.Concurrent;
 using System.Configuration;
 using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Audio;
@@ -10,57 +9,60 @@ using NAudio.Wave;
 
 namespace SteakBot.Core.Services
 {
-    public class AudioService
+    public class AudioTuple : IDisposable
     {
-        private readonly ConcurrentDictionary<ulong, List<IAudioClient>> _connectedChannelsPerServer;
+        public IAudioClient AudioClient;
 
-        private bool _isPlaying;
-        private AudioOutStream _audioOutStream;
+        public IVoiceChannel VoiceChannel;
 
-        public AudioService(IDiscordClient discordClient)
+        public AudioOutStream AudioStream;
+
+        public bool IsPlaying;
+
+        public AudioTuple(IAudioClient audioClient, IVoiceChannel voiceChannel, AudioOutStream audioStream)
         {
-            var dict = discordClient.GetGuildsAsync().Result.ToDictionary(x => x.Id, y => new List<IAudioChannel>());
-            foreach (var kvp in dict)
-            {
-                _connectedChannelsPerServer.AddOrUpdate(kvp.Key, kvp.Value,
-                    (guildId, channels) =>
-                    {
-                        return _connectedChannelsPerServer[guildId].Union(channels).ToList();
-                    });
-            }
-            _connectedChannelsPerServer = new ConcurrentDictionary<ulong, IList<IAudioClient>>(new Dictionary<ulong, IList<IAudioChannel>>());
-            ConcurrentDictionary<int, int> asdf = new ConcurrentDictionary<int, int>(new Dictionary<int, int>());
+            AudioClient = audioClient;
+            VoiceChannel = voiceChannel;
+            AudioStream = audioStream;
+            IsPlaying = false;
         }
 
-        public async Task JoinAudioChannel(IGuild guild, IVoiceChannel target)
+        public void Dispose()
         {
-            if (target.Guild.Id != guild.Id)
+            IsPlaying = false;
+            AudioStream?.Dispose();
+            AudioStream = null;
+            AudioClient?.StopAsync();
+            //AudioClient?.Dispose();
+        }
+    }
+
+    public class AudioService : IDisposable
+    {
+        private readonly ConcurrentDictionary<ulong, AudioTuple> _audioSetupPerServer = new ConcurrentDictionary<ulong, AudioTuple>();
+
+        public async Task JoinAudioChannel(IGuild guild, IVoiceChannel targetVoiceChannel)
+        {
+            if (targetVoiceChannel.Guild.Id != guild.Id)
             {
                 return;
             }
 
-            if (_connectedChannelsPerServer.ContainsKey(guild.Id))
+            if (_audioSetupPerServer.ContainsKey(guild.Id))
             {
                 return;
             }
 
-            var audioClient = await target.ConnectAsync();
-            //if (_connectedChannelPerServer.TryAdd(guild.Id, audioClient))
-            {
-                // If you add a method to log happenings from this service,
-                // you can uncomment these commented lines to make use of that.
-                //await Log(LogSeverity.Info, $"Connected to voice on {guild.Name}.");
-            }
+            var audioClient = await targetVoiceChannel.ConnectAsync();
+            var audioStream = audioClient.CreatePCMStream(AudioApplication.Voice);
+            _audioSetupPerServer.TryAdd(guild.Id, new AudioTuple(audioClient, targetVoiceChannel, audioStream));
         }
 
         public async Task LeaveAudioChannel(IGuild guild)
         {
-            _isPlaying = false;
-
-            if (_connectedChannelPerServer.TryRemove(guild.Id, out IAudioClient client))
+            if (_audioSetupPerServer.TryRemove(guild.Id, out var audioTuple))
             {
-                await client.StopAsync();
-                //await Log(LogSeverity.Info, $"Disconnected from voice on {guild.Name}.");
+                audioTuple.Dispose();
             }
         }
 
@@ -86,32 +88,41 @@ namespace SteakBot.Core.Services
 
         private async Task SendAudioAsync(IGuild guild, string filePath)
         {
-            if (_isPlaying)
+            if (!_audioSetupPerServer.TryGetValue(guild.Id, out var audioTuple))
             {
                 return;
             }
 
-            if (_connectedChannelPerServer.TryGetValue(guild.Id, out IAudioClient audioClient))
+            if (audioTuple.IsPlaying)
             {
-                _audioOutStream = _audioOutStream ?? audioClient.CreatePCMStream(AudioApplication.Voice);
-                using (var mp3 = new Mp3FileReader(filePath))
-                using (var pcmStream = WaveFormatConversionStream.CreatePcmStream(mp3))
+                return;
+            }
+
+            using (var mp3 = new Mp3FileReader(filePath))
+            using (var pcmStream = WaveFormatConversionStream.CreatePcmStream(mp3))
+            {
+                try
                 {
-                    try
-                    {
-                        _isPlaying = true;
-                        await pcmStream.CopyToAsync(_audioOutStream);
-                    }
-                    finally
-                    {
-                        await _audioOutStream.FlushAsync();
-                        await pcmStream.FlushAsync();
-                        _isPlaying = false;
-                    }
+                    audioTuple.IsPlaying = true;
+                    await pcmStream.CopyToAsync(audioTuple.AudioStream);
+                }
+                finally
+                {
+                    await audioTuple.AudioStream.FlushAsync();
+                    await pcmStream.FlushAsync();
+                    audioTuple.IsPlaying = false;
                 }
             }
         }
 
         #endregion
+
+        public void Dispose()
+        {
+            foreach (var kvp in _audioSetupPerServer)
+            {
+                kvp.Value.Dispose();
+            }
+        }
     }
 }
