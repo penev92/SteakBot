penev92/SteakBot

commit 7e84479922e18bae06f15d81dbfc565163438d97

Fixed a bug with manual quoting using the wrong channel to look for author and timestamp of the quote

SteakBot.Core/Modules/QuoteModule.cs
@@ -278,7 +278,7 @@ namespace SteakBot.Core.Modules
                 lines.RemoveAt(0);
             }
 
-            if (TryGetQuoteAuthorAndTimestamp(lines[0].Trim(), Context.Channel, out var author, out var authorName, out var timestamp))
+            if (TryGetQuoteAuthorAndTimestamp(lines[0].Trim(), referredChannel, out var author, out var authorName, out var timestamp))
             {
                 lines.RemoveAt(0);
             }

commit 97fca8fa1fb509d69c61c770991a31166b5a714b

Fixed a potential NRE with manual QuotingService
This fixes a regression from 7e84479922e18bae06f15d81dbfc565163438d97

SteakBot.Core/Modules/QuoteModule.cs
@@ -200,7 +200,7 @@ namespace SteakBot.Core.Modules
 
         private async void HandleQuote(SocketGuild guild, string message)
         {
-            var embed = _quotingService.CreateEmbed(guild, message);
+            var embed = _quotingService.CreateEmbed(guild, message, Context.Channel);
             await Context.Channel.DeleteMessageAsync(Context.Message, RequestOptions.Default);
             await SendQuote(Context.User, embed);
         }

SteakBot.Core/Services/QuotingService.cs
@@ -103,7 +103,7 @@ namespace SteakBot.Core.Services
             return messages;
         }
 
-        public Embed CreateEmbed(SocketGuild guild, string message)
+        public Embed CreateEmbed(SocketGuild guild, string message, IChannel fallbackChannel)
         {
             var lines = message.Split('\n').ToList();
 
@@ -112,7 +112,7 @@ namespace SteakBot.Core.Services
                 lines.RemoveAt(0);
             }
 
-            if (TryGetQuoteMetadata(lines[0].Trim(), referredChannel, out var quoteMetadata))
+            if (TryGetQuoteMetadata(lines[0].Trim(), referredChannel ?? fallbackChannel, out var quoteMetadata))
             {
                 lines.RemoveAt(0);
             }