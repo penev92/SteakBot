147bf109d9fd438f5d7cd6c3c229605970e6c581
Shavarsh Movsesyan <naps250@gmail.com>
2020-06-20 23:07:18 +0300

    Added several simple commands Added global Random instance

---
+++ SteakBot.Core/ThreadLocalRandom.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+namespace SteakBot.Core
+{
+    // https://codeblog.jonskeet.uk/2009/11/04/revisiting-randomness/
+    /// <summary>
+    /// Convenience class for dealing with randomness.
+    /// </summary>
+    public static class ThreadLocalRandom
+    {
+        /// <summary>
+        /// Random number generator used to generate seeds,
+        /// which are then used to create new random number
+        /// generators on a per-thread basis.
+        /// </summary>
+        private static readonly Random globalRandom = new Random(Guid.NewGuid().GetHashCode());
+        private static readonly object globalLock = new object();
+
+        /// <summary>
+        /// Random number generator
+        /// </summary>
+        private static readonly ThreadLocal<Random> threadRandom = new ThreadLocal<Random>(NewRandom);
+
+        /// <summary>
+        /// Creates a new instance of Random. The seed is derived
+        /// from a global (static) instance of Random, rather
+        /// than time.
+        /// </summary>
+        public static Random NewRandom()
+        {
+            lock (globalLock)
+            {
+                return new Random(globalRandom.Next());
+            }
+        }
+
+        /// <summary>
+        /// Returns an instance of Random which can be used freely
+        /// within the current thread.
+        /// </summary>
+        public static Random Instance { get { return threadRandom.Value; } }
+    }
+}
